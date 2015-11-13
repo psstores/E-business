bbb9dac1085bc87d10f51f0f3eba1c67adb18e0c
psstores <2669578421@qq.com>
2015-11-13 11:09:46 +0800

    sample data

--- wechat.js
+++ wechat.js
@@ -1,11 +1,12 @@
-phantom.outputEncoding="gb2312"
+// phantom.outputEncoding="utf-8";
+phantom.outputEncoding="GBK";
 var fs=require('fs')
 var casper=require("casper").create({
-  verbose:true
+  // verbose:true
 });
 var data = fs.read("cookies.txt")
 phantom.cookies = JSON.parse(data)
-var url="http://weixin.sogou.com/gzh?openid=oIWsFtyfVeeK61kmJmfksHmtdfw0&ext=Uc8g8yaxQpF9wzNNDd71426MmuON278tLmPjdKofRHx_4qgkcyEfKliwCT-M6iB_"
+var url="http://weixin.sogou.com/gzh?openid=oIWsFtyfVeeK61kmJmfksHmtdfw0&ext=zMKRwhGG3zaXkD238Hpi7Dv5JARbIXAptZtdMuDCE8w0iDg_1HKjndj39KH0xLdZ"
 function a(){
   
       var d=document.getElementById('wxmore').getAttributeNode('style').value
@@ -14,22 +15,24 @@ function a(){
 function b(){
   casper.wait(5000,function(){
    c=casper.evaluate(a);
-  casper.echo(c)
+  // casper.echo(c)
   casper.capture('test.png')
-  // fs.write('index.html',casper.getHTML(),'w')
+  // 
             if(c=="visibility: visible;"){
               this.click('div#wxmore a')
-              this.echo('正在翻页>>>>>>')
+              // this.echo('正在翻页>>>>>>')
               b()
             }else{
               casper.capture('all.png')
+              this.echo(this.getHTML())
+              fs.write('index.html',casper.getHTML(),'w')
               this.exit()
             }
       })
   
 }
 casper.start(url,function(){
-  console.log("test is starting")
+  // console.log("test is starting")
 })
 casper.then(b)
 casper.run(function(){
